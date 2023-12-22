246cd0ba63338abeaadefecb8588ac237f83267b
Camilo Andres Romero Lopez <camiloandres.romero@globant.com>
2023-12-22 12:39:46 -0500

    test case service

--- src/test/java/com/milo/springaws/service/impl/SolutionServiceImplTest.java
+++ src/test/java/com/milo/springaws/service/impl/SolutionServiceImplTest.java
@@ -1,11 +1,27 @@
 package com.milo.springaws.service.impl;
 
+import com.milo.springaws.service.SolutionService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 
 import static org.junit.jupiter.api.Assertions.*;
+
 @ContextConfiguration(classes = {SolutionServiceImpl.class})
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 class SolutionServiceImplTest {
+
+    @Autowired
+    SolutionService solutionService;
+
+    @Test
+    void test() {
+        Integer[] values = {2, 7, 9, 3, 1};
+        var expected = -1;
+        var actual = assertDoesNotThrow(() -> solutionService.rob(values));
+
+        assertAll(() -> assertEquals(expected, actual));
+    }
 
 }

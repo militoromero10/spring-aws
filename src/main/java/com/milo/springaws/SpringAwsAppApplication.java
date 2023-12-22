bb71d1af8113a904271ab9747480d74c0e3963e7
Camilo Andres Romero Lopez <camiloandres.romero@globant.com>
2023-12-22 11:51:01 -0500

    init project

---
+++ src/main/java/com/milo/springaws/SpringAwsAppApplication.java
@@ -0,0 +1,13 @@
+package com.milo.springaws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAwsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAwsAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/milo/springaws/SpringAwsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.milo.springaws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAwsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
